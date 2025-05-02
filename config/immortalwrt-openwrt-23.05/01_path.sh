4486947af3b1a8049fa527643e2d292e1e318906
iFHax <ifhax@proton.me>
2025-05-03 05:49:08 +0800

    Delete config/immortalwrt-openwrt-23.05 directory

--- config/immortalwrt-openwrt-23.05/01_path.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-# other
-# rm -rf package/emortal/{autosamba,ipv6-helper}
-
