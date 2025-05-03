07d4bd82b37d07d7c16b847184cee29a2051c70e
iFHax <ifhax@proton.me>
2025-05-03 22:33:40 +0800

    Update 01_path.sh

--- config/immortalwrt-openwrt-24.10/01_path.sh
+++ config/immortalwrt-openwrt-24.10/01_path.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Add a feed source
-echo "src-git modem https://github.com/intannajwa/modem_immo.git" >> feeds.conf.default
+# echo "src-git modem https://github.com/intannajwa/modem_immo.git" >> feeds.conf.default
 
