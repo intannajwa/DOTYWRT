11954646ace1b4ef0c511d0458723b1cad721ec7
xBinPDA <nazren3git@gmail.com>
2025-05-22 13:52:41 +0800

    Update 02_path.sh

--- config/openwrt-ipq/02_path.sh
+++ config/openwrt-ipq/02_path.sh
@@ -1,4 +1,1 @@
 #!/bin/bash
-
-sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
-echo "DISTRIB_SOURCECODE='ImmortalWRT'" >>package/base-files/files/etc/openwrt_release
